363b5b0cf363154c5ae74519f85b8584750aa20c
Stevens Qiu <stevensqiu@gmail.com>
2024-11-13 21:04:21 -0800

    Remove var.function_name from description of ram_role_arn output
    Variables are not allowed to be specified in output.

--- FunctionComputeV3Service/outputs.tf
+++ FunctionComputeV3Service/outputs.tf
@@ -1,4 +1,4 @@
 output "ram_role_arn" {
-  description = "The RAM role ARN that is assigned to the ${var.function_name} Function Compute service."
+  description = "The RAM role ARN that is assigned to the Function Compute service."
   value       = var.function_role_arn != null ? var.function_role_arn : alicloud_ram_role.default_ram_role[0].arn
 }
